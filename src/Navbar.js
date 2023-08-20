e506ad9fe052b7706eba5d4ba2c5b5f32b87cbc3
Claire Devlin <clairebeargoldielox@icloud.com>
2023-08-20 08:57:17 -0700

    begin navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const navigate = useNavigate();
+    
+    const logout = () => {
+
+    }
+
     return <div id="navbar">
-        
+        <Link to={"/"}> Crib Crazy </Link>
+        {!cookies.access_token ? 
+            <Link to={"/auth"}> Login </Link>
+         : 
+            <button onClick={logout}>Logout</button>
+        }
     </div>
 }
 
